a685d54510761fbc3b070faf15b6e75bcf259882
ricky_000 <ricky_000@192.168.1.33>
2017-03-03 14:06:26 +0100

    Proyecto Rueta

---
+++ src/proyectoruleta/Ruleta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoruleta;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ricky_000
+ */
+public class Ruleta {  
+    ArrayList<Integer> rulet = new ArrayList<Integer>();
+    
+}
